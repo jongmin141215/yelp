7f652010ced2e9cbc16ca58fd50cccee561b20e1
Jongmin Kim <kjm141215@gmail.com>
2015-10-12 23:06:35 +0100

    add update method

--- yelp/app/controllers/restaurants_controller.rb
+++ yelp/app/controllers/restaurants_controller.rb
@@ -1,23 +1,35 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
-    p params
+    p 'index ', params
   end
 
   def new
     @restaurant = Restaurant.new
-    p params
+    p 'new ', params
   end
 
   def create
     Restaurant.create(restaurant_params)
-    p params
+    p 'create ', params
     redirect_to '/restaurants'
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    p params
+    p 'show ', params
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    p 'edit ', params
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    p 'update ', params
+    redirect_to '/restaurants'
   end
 
   private
