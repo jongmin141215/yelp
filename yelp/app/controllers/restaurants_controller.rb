93ef37ea9f29f0aa019bf1daf79ae652352f81d5
Jongmin Kim <kjm141215@gmail.com>
2015-10-12 22:47:28 +0100

    Add description & show method

--- yelp/app/controllers/restaurants_controller.rb
+++ yelp/app/controllers/restaurants_controller.rb
@@ -1,16 +1,23 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    p params
   end
 
   def new
-
+    @restaurant = Restaurant.new
+    p params
   end
 
   def create
     Restaurant.create(restaurant_params)
     p params
     redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    p params
   end
 
   private
